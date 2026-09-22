959fe1328f257daab23296c8d027e7595bb7b0ea
Mansi <142657614+mansi-0603@users.noreply.github.com>
2026-09-23 00:40:48 +0530

    Time: 0 ms (100%), Space: 12.8 MB (61.53%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int> nextGreater;
         stack<int> st;
+        unordered_map<int, int> trackNGE;
 
-        for(int i = nums2.size()-1; i >= 0; i--){
-            while(!st.empty() && st.top() <= nums2[i]){
+        for (int i = nums2.size() - 1; i >= 0; i--) {
+
+            while (!st.empty() && st.top() <= nums2[i]) {
                 st.pop();
             }
-            if(st.empty()) nextGreater[nums2[i]] = -1;
-            else nextGreater[nums2[i]] = st.top();
+            if (st.empty()) {
+                trackNGE[nums2[i]] = -1;
+            } else {
+                trackNGE[nums2[i]] = st.top();
+            }
             st.push(nums2[i]);
         }
-        vector<int>ans;
-        for(int i = 0; i < nums1.size(); i++){
-            ans.push_back(nextGreater[nums1[i]]);
+
+        vector<int> ans;
+        for (int i = 0; i < nums1.size(); i++) {
+            ans.push_back(trackNGE[nums1[i]]);
         }
         return ans;
     }
